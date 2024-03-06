3b00aa598fbbc84047feaa42b062ff715f59de88
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-03-06 12:21:17 +0400

    Mentor Lesson 5

---
+++ src/lesson6/Mentor/PalindromeChecker.java
@@ -0,0 +1,21 @@
+package lesson6.Mentor;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type any number: ");
+        String num = scan.nextLine();
+        String reverseNum = "";
+        int numLength = num.length();
+        for (int i = (numLength - 1); i >= 0; i--) {
+            reverseNum = reverseNum + num.charAt(i);
+        }
+        if (num.equals(reverseNum)) {
+            System.out.println(num + " is Palindrome number.");
+        } else {
+            System.out.println(num + " is not Palindrome number.");
+        }
+    }
+}
