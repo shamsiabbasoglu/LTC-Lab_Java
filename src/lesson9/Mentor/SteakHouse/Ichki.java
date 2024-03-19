58760a1255bfca8ff93e69f41054c8525d5d48ab
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-03-19 12:53:11 +0400

    Mentor Lesson 9

---
+++ src/lesson9/Mentor/SteakHouse/Ichki.java
@@ -0,0 +1,27 @@
+package lesson9.Mentor.SteakHouse;
+
+public class Ichki {
+    String name;
+    double price;
+
+    public Ichki(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ichki{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
