d70fa91336feda29f2a84e13d348470f523ace9f
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-04-15 13:05:44 +0400

    Polymorphism, overload method, compile-time and run-time errors.

--- src/lesson14/Main/StringUtil/Main.java
+++ src/lesson14/Main/StringUtil/Main.java
@@ -4,16 +4,20 @@ public class Main {
     public static void main(String[] args) {
         StringUtil stringUtil = new StringUtil();
 
-        // Test concatenate method
-        String concatenated = stringUtil.manipulate("Hello, ", "World!");
-        System.out.println("Concatenated string: " + concatenated);
-
         // Test concatenate method with three strings
-        String concatenatedThree = stringUtil.manipulate("Hello, ", "Beautiful ", "World!");
-        System.out.println("Concatenated three strings: " + concatenatedThree);
+        String concatenated = stringUtil.manipulate("Hello, ", "Beautiful ", "World!");
+        System.out.println("Concatenated three string: " + concatenated);
+
+        // Test concatenate method with four strings
+        String concatenatedThree = stringUtil.manipulate("Hello, ", "Beautiful ", "Great ", "World!");
+        System.out.println("Concatenated four strings: " + concatenatedThree);
 
         // Test toUpperCase method
-        String upperCase = stringUtil.manipulate("hello");
+        String upperCase = stringUtil.manipulate("hello, ", "world!");
         System.out.println("Uppercase string: " + upperCase);
+
+        // Test reverse method
+        String reversal = stringUtil.manipulate("hello");
+        System.out.println("Reversed string: " + reversal);
     }
 }
