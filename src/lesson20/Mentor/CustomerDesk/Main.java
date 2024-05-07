fcec6042fd9ad5a735db4fa47614877c266dc9b7
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-05-07 12:23:24 +0400

    Generics, Collections part 1

--- src/lesson20/Mentor/CustomerDesk/Main.java
+++ src/lesson20/Mentor/CustomerDesk/Main.java
@@ -1,17 +1,18 @@
 package lesson20.Mentor.CustomerDesk;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Customer[] customers = new Customer[4];
+        List<Customer> customers = new ArrayList<>();
 
-        customers[0] = new Customer("Abraham", true);
-        customers[1] = new Customer("Moses", false);
-        customers[2] = new Customer("Jonas", true);
-        customers[3] = new Customer("David", false);
+        customers.add(new Customer("Abraham", true));
+        customers.add(new Customer("Moses", false));
+        customers.add(new Customer("Jonas", true));
+        customers.add(new Customer("David", false));
 
         List<String> senders = new LinkedList<>();
         List<String> receivers = new LinkedList<>();
