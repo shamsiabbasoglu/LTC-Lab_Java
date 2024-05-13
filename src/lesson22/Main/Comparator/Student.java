dbc7cd8c5b610a65888af0eb6e57c7c4b469d258
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-05-13 06:50:18 +0400

    Comparable, Comparator

---
+++ src/lesson22/Main/Comparator/Student.java
@@ -0,0 +1,34 @@
+package lesson22.Main.Comparator;
+
+public class Student {
+    private String name;
+    private int age;
+    private int grade;
+
+    public Student(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", grade=" + grade +
+                '}';
+    }
+}
